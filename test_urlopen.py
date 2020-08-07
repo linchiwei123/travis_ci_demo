f5ae459295a956067ced91dd29f2f2b8a6c3bc8f
linchiwei123 <40888469+linchiwei123@users.noreply.github.com>
2020-08-07 14:24:49 +0800

    urlopen https://matplotlib.org

--- test_urlopen.py
+++ test_urlopen.py
@@ -2,5 +2,7 @@ import urllib.request
 
 
 def test_urlopen():
-    r = urllib.request.urlopen("https://www.google.com")
-    r.read()
+    with urllib.request.urlopen("https://www.google.com") as r:
+        r.read()
+    with urllib.request.urlopen("https://matplotlib.org/1.5.0/_static/logo2.png") as r2:
+        r2.read()
