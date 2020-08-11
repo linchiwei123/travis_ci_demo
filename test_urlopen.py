e0b4d3c1ccef6bb4e420f8ca1ffb6678ff062972
linchiwei123 <40888469+linchiwei123@users.noreply.github.com>
2020-08-11 12:31:28 +0800

    pytest parametrize

--- test_urlopen.py
+++ test_urlopen.py
@@ -1,8 +1,12 @@
 import urllib.request
 
+import pytest
 
-def test_urlopen():
-    with urllib.request.urlopen("https://www.google.com") as r:
+
+@pytest.mark.parametrize(
+    "url",
+    ["https://www.google.com", "https://matplotlib.org/1.5.0/_static/logo2.png"]
+)
+def test_urlopen(url):
+    with urllib.request.urlopen(url) as r:
         r.read()
-    with urllib.request.urlopen("https://matplotlib.org/1.5.0/_static/logo2.png") as r2:
-        r2.read()
